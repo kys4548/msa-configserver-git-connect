c40a822d2d866132876b5c45818d03fb63b3472d
youngsil <kys4548@gmail.com>
2020-07-27 22:55:22 +0900

    properties modified

--- microservice-a/src/main/java/com/youngsil/microservicea/ApplicationConfiguration.java
+++ microservice-a/src/main/java/com/youngsil/microservicea/ApplicationConfiguration.java
@@ -2,11 +2,13 @@ package com.youngsil.microservicea;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties("application")
+@RefreshScope
 @Data
 public class ApplicationConfiguration {
 
