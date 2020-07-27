1462fa20e5a8ef49faf2c4d7dea074772311c561
youngsil <kys4548@gmail.com>
2020-07-27 22:46:22 +0900

    add refresh endpoint

--- microservice-a/src/main/java/com/youngsil/microservicea/MicroserviceAApplication.java
+++ microservice-a/src/main/java/com/youngsil/microservicea/MicroserviceAApplication.java
@@ -2,8 +2,10 @@ package com.youngsil.microservicea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class MicroserviceAApplication {
 
     public static void main(String[] args) {
